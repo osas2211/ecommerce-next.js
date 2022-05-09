1b4994913b962d4774f36ebaf6bde825facca3ad
osas2211 <osaretin.frank10@gmail.com>
2022-05-09 10:32:13 +0100

    initialized firebase__

--- pages/index.js
+++ pages/index.js
@@ -3,10 +3,8 @@ import Featured from "../components/Featured_"
 import ChooseUs from "../components/ChooseUs"
 import BestSellers from "../components/BestSellers"
 import Hero from "../components/Hero"
-import { app } from "../firebase/firebase"
 
 export default function Home() {
-  console.log(app)
   return (
     <div className="home">
       <HomeHero />
